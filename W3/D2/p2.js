3ed7667b6e8b48abaae7a574f0ad0a07f7ec0b00
anjuvmyageri-beep <anjuvmyageri@gmail.com>
2026-03-03 15:43:01 +0530

    Comparison  operators added

---
+++ W3/D2/p2.js
@@ -0,0 +1,10 @@
+//Comparison operators
+console.log("Comparison operators");
+const x=4,y=5;
+console.log("x==y: ",x==y);//Loose equality
+console.log("x===y: ",x===y);//Strict equality
+console.log("x!=y: ",x!=y);//Loose inequality
+console.log("x>3: ",x>3);//Strict inequality
+console.log("x<3: ",x<3);
+console.log("x>=3: ",x>=3);
+console.log("x<=3: ",x<=3);
