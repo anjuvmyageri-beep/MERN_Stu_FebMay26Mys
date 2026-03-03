2d5c02b939890543c0ae105910a16e600b9aaa44
anjuvmyageri-beep <anjuvmyageri@gmail.com>
2026-03-03 20:03:46 +0530

    Function declaration added

---
+++ W3/D2/p5.js
@@ -0,0 +1,9 @@
+//Funstion declaration
+//Function greet(name);
+//Function Declaration/definition
+greet("Anjali");
+function greet(name){
+    console.log("Hello,"+name+" !");
+}
+//Function
+greet("Anjali");
