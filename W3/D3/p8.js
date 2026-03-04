2d65558982d6c7408bd4ee0391fcfdce6e149efb
anjuvmyageri-beep <anjuvmyageri@gmail.com>
2026-03-04 14:35:03 +0530

    json stringify added

--- W3/D3/p8.js
+++ W3/D3/p8.js
@@ -1,8 +1,14 @@
 //json stringify parse
 const employee={
     id:101,
-    name:"Naveen",
+    name:"Anjali",
     dpt:"MCA",
     isActive:true
 };
+const jsonString=JSON.stringify(employee);
+console.log(jsonString);
 console.log(employee);
+
+//JSON parsing
+const parseObject=JSON.parse(jsonString);
+console.log(parseObject);
