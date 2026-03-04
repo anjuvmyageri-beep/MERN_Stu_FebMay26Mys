f11d39b4499a856e8d0ea1b3afe1191b561805fb
anjuvmyageri-beep <anjuvmyageri@gmail.com>
2026-03-04 13:22:27 +0530

    break statement added

---
+++ W3/D3/p3.js
@@ -0,0 +1,17 @@
+
+for(let i=0;i<5;i++){ //break statement
+    if(i===2){
+        console.log("i",i);
+        break;
+    }
+}
+
+//continue
+ for(let i=0;i<5;i++){ //
+    if(i===2){
+        console.log("i",i);
+        continue;
+    }
+    console.log("i",i);
+
+}
