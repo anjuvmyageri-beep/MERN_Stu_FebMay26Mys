4e8078a5bfbf7d0e23dd0a1d7d68cecbaabcad33
anjuvmyageri-beep <anjuvmyageri@gmail.com>
2026-03-04 14:24:50 +0530

    Basic of objects added

--- W3/D3/p5.js
+++ W3/D3/p5.js
@@ -5,5 +5,22 @@ const person={
     isStudent:false
 };
 //console.log("person",person);
-console.log("Name:",person.name);
-console.log("Age:", person["age"]);
+//console.log("Name:",person.name);
+//console.log("Age:", person["age"]);
+
+//Add a new property
+person.city="Mysore";
+console.log("person",person);
+
+//Modify
+person.age=31;
+//delete
+delete person.isStudent;
+console.log("person",person);
+
+//object constructor
+const car=new Object();
+car.make="Audi";
+car.model="A4";
+car.year=2026;
+console.log("car",car);
