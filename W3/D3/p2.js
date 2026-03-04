980f0f77abf3b2add2d417478e512198ce76d546
anjuvmyageri-beep <anjuvmyageri@gmail.com>
2026-03-04 13:21:40 +0530

    While loop added

---
+++ W3/D3/p2.js
@@ -0,0 +1,27 @@
+//While loop
+console.log("while loop");
+
+//forward while loop
+let count=0;
+while (count<5){
+    console.log("count: ",count);
+    count++;
+}
+//Backward while loop
+let i=20;
+while(i<10){
+    console.log("i: ",i);
+    i--;
+}
+//do while
+let value=0;
+do{
+    console.log("value :",value);
+    i++;
+}while(value<3);
+
+
+
+
+
+
