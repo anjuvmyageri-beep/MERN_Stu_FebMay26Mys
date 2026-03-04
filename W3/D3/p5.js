9bc6c82ab36aa3c27d656b48e66bae8fbb926eff
anjuvmyageri-beep <anjuvmyageri@gmail.com>
2026-03-04 13:24:06 +0530

    Basics of objects added

---
+++ W3/D3/p5.js
@@ -0,0 +1,9 @@
+//Basics of Objects
+const person={
+    name:"Anjali",
+    age:23,
+    isStudent:false
+};
+//console.log("person",person);
+console.log("Name:",person.name);
+console.log("Age:", person["age"]);
