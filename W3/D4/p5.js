ff6d897d104a903ba6cdc4e4d9eee6dc0c62f33e
anjuvmyageri-beep <anjuvmyageri@gmail.com>
2026-03-05 14:53:23 +0530

    Reduce method added

---
+++ W3/D4/p5.js
@@ -0,0 +1,16 @@
+//,Reduce method
+let nums=[5,10,15];
+
+//let total=nums.reduce((intermediateSum,current)=>intermediateSum+current,0);
+//let avg = total / nums.length;
+
+//console.log(total);
+//console.log(avg);
+
+//Reduce to object count by category
+let items=["pen","pencil","pen","eraser"];
+let count=items.reduce((a,b)=>{
+    a[b]=(a[b]||0)+1;
+    return a;
+},{});
+console.log("Item count:",count);
