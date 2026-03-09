db073816571675824aff978ea704549e5216d91c
anjuvmyageri-beep <anjuvmyageri@gmail.com>
2026-03-09 21:26:24 +0530

    Wallet with ‘this’ keyword added

---
+++ W3/D3/Assignment5.js
@@ -0,0 +1,14 @@
+const wallet = {
+  owner: "Anjali",
+  balance:2000,
+  lastTransaction: null,
+
+  deposit: function(amount) { 
+  },
+  withdraw: function(amount) {
+  }
+};
+wallet.deposit(500);
+wallet.withdraw(200);
+
+console.log(wallet);
