d30aa05a3beb7dc82b2315562c54f50ae0f000b9
anjuvmyageri-beep <anjuvmyageri@gmail.com>
2026-03-09 19:52:16 +0530

    Build a Compact Invoice Using Rest + Default added

---
+++ W3/D3/Assignment2.js
@@ -0,0 +1,39 @@
+function invoice(gstRate = 0.18, ...items) {
+    let subtotal = 0;
+
+    for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+
+        if (item.name === "STOP") {
+            break;
+        }
+
+        // Ignore invalid items
+        if (!item.price || !item.qty || item.price <= 0 || item.qty <= 0) {
+            continue;
+        }
+
+        // Add valid item total
+        subtotal += item.price  *item.qty;
+    }
+
+    const gst = subtotal * gstRate;
+    const total = subtotal + gst;
+
+    return {
+        subtotal,
+        gst,
+        total
+    };
+}
+
+const result = invoice(
+    0.18,
+    { name: "Pen", price: 10, qty: 3 },
+    { name: "Notebook", price: 50, qty: 2 },
+    { name: "Invalid", price: -5, qty: 1 }, 
+    { name: "STOP", price: 100, qty: 1 },   
+    { name: "Pencil", price: 5, qty: 10 } 
+);
+
+console.log(result);
