6ff52083b409345ba319017aa5058573109aa404
anjuvmyageri-beep <anjuvmyageri@gmail.com>
2026-03-10 16:43:52 +0530

    Dom Introduction added

--- W4/D1/p7.js
+++ W4/D1/p7.js
@@ -1,33 +1,41 @@
-//grtElementById
-// console.log("document object:", document);
-//console.log("page title:", document.title);
+console.log("Document Object",document);
+console.log("page title",document.title);
 
-//const heading = document.getElementById("mainHeading");
-//console.log("heading text:", heading.textContent);
+//const heading = document.getElementById("MainHeading");
+// console.log("Heading text",heading.textContent);
 
-//getElementByClassName
-const info =document.getElementsByClassName("info");
+//getElementsByClassName
+const info = document.getElementsByClassName("info");
 const run = document.getElementById("run");
 
-
-//getelementbytagname
+//
 const spanTag = document.getElementsByTagName("span");
-run.addEventListener("click",function(){
-    for(let i=0; i<info.length; i++){
-        console.log(info[i].textContent);
-        info[i].computedStyleMap.color = "blue";
-    }
-    for(let i=0; i<spanTag.length; i++){
-        spanTag[i].style.backgroundColor ="lightgreen";
-    }
-    //Query selector: returns the first elements matching a css selector    
 
-const mainFirstHeading = document.querySelector(".mainHeading");
-mainFirstHeading.style.color = "red";
+
+run.addEventListener("click",function(){
+    //querry selector:returns the first element matchs the css selector
+    const mainFirstHeading = document.querySelector(".mainHeading");
+    mainFirstHeading.style.color = "red";
+    
+    for(let i=0;i<info.length;i++){
+        console.log(info[i].textContent);
+        info[i].style.color = "blue";
+    }
+
+
+    for(let i=0;i<spanTag.length;i++){
+        console.log(spanTag[i].textContent);
+        spanTag[i].style.backgroundColor = "lightgreen";
+    }
+    for(let i=0;i<mainFirstHeading.length;i++){
+        console.log(mainFirstHeading[i].textContent);
+        mainFirstHeading[i].style.backgroundColor = "lightgreen";
+    }
 });
-//Query selector all:returns all element matching the selector
-const task = document.querySelectorAll(".task");
+
+//query selector all: returns all elements matching the selector
+const tasks = document.querySelectorAll(".task");
 //task.style.color = "purple";
-task.forEach(function(task){
-task.style.color = "purple";
+tasks.forEach(function(task){
+    task.style.color = "purple";
 })
