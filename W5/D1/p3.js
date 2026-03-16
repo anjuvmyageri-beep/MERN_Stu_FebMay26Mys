7338de2e9584f772fa981b9a2fada4cdd96cb57d
anjuvmyageri-beep <anjuvmyageri@gmail.com>
2026-03-16 14:41:55 +0530

    Architecture demo added

---
+++ W5/D1/p3.js
@@ -0,0 +1,9 @@
+console.log("Node JS Architecture demo");
+console.log("1.script started");
+
+//setTimeOut
+setTimeout(()=>{
+    console.log("3.Timer callback finished after waiting");
+},1000);
+
+console.log("2.script continued without waiting for timer callback")
