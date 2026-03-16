7d579b6609e40880b8013c8f9a2e19865fb4b970
anjuvmyageri-beep <anjuvmyageri@gmail.com>
2026-03-16 14:19:26 +0530

    Introduction to Node

--- W5/D1/p1.js
+++ W5/D1/p1.js
@@ -2,4 +2,17 @@
 
 const runtimeName="Node.js";
 console.log("Intoduction to Node.js");
-//console.log(${runtimeName}runs javascript outside the browser);
+console.log(`${runtimeName} runs javascript outside the browser`);
+
+const commonUses=[
+    " used for server-side app",
+    "automation scripts can be created"
+];
+
+//array destructuring
+//console.log(commonUses[0]);
+//console.log(commonUses[1]);
+commonUses.forEach((commonUse,index)=>{
+    console.log(`${index+1}.${commonUse}`);
+});
+
