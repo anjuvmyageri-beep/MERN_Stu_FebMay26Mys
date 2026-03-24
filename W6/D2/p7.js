ba481c99376aa390bd6d7462997a55bf8b8c67fc
anjuvmyageri-beep <anjuvmyageri@gmail.com>
2026-03-24 16:59:00 +0530

    setImmediate vs aetTimeout

---
+++ W6/D2/p7.js
@@ -0,0 +1,12 @@
+//setImmediate vs setTimeout
+console.log("Scheduling setTimeout and setImmediate");
+
+//callback timer
+setTimeout(()=>{
+    console.log("Timer callback from setTimeout");
+},0);
+
+// setImmediate callback
+setImmediate(function(){
+    console.log("Both callbacks are now waiting for the event loop");
+});
